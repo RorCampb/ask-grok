4d03e9f72edbb96ffa25eb742837f332205b897e
Rory Campbell <rorycampbell2053@gmail.com>
2025-08-31 13:26:57 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,122 @@
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::collections::HashMap;
+use reqwest::Client;
+use reqwest_eventsource::{EventSource, Event};
+use std::env;
+use futures_util::StreamExt;
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ToolCall  {
+    name: String,
+    description: String,
+    parameters: Parameters,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Parameters {
+    r#type: String,
+    properties: HashMap<String, Properties>,
+    required: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Properties {
+    r#type: String,
+    description: String,
+}
+
+
+#[derive(Debug, Serialize)]
+struct Body {
+    model: String,
+    messages: Vec<Messages>, 
+    stream: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Messages {
+    role: String,
+    content: String, 
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    id: String,
+    object: String,
+    created: u32,
+    model: String,
+    choices: Vec<Choices>,
+    system_fingerprint: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Delta {
+    content: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Choices {
+    index: u32,
+    delta: Delta,
+}
+
+fn ask_grok(client: &Client, api_key: &str, request: &Body) -> EventSource {
+    let grok_response = client
+        .post("https://api.x.ai/v1/chat/completions")
+        .bearer_auth(api_key)
+        .header("Content-Type", "application/json")
+        .json(request);
+
+    EventSource::new(grok_response).expect("Failed to start events...")
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let api_key = env::var("XAI_API_KEY")?;
+    let client = Client::new();
+
+    let request_body: Body = Body {
+        messages: vec![
+            Messages {
+                role: "user".into(),
+                content: "Hello! Dont say much".into(),
+            },
+        ],
+        stream: true,
+        model: "grok-4".to_string() 
+    }; 
+
+    let mut es = ask_grok(&client, &api_key, &request_body);
+
+    while let Some(event) = es.next().await {
+        match event {
+            Ok(Event::Open) => {}
+
+            Ok(Event::Message(msg)) => {
+                if msg.data == "[DONE]" {
+                    break;
+                }
+
+                let delta: Data = serde_json::from_str(&msg.data).expect("Failed to read delta");
+                if let Some(chunk) = delta
+                    .choices
+                    .get(0)
+                    .and_then(|c| c.delta.content.as_deref()) 
+                {
+                    println!("{chunk}");
+                } else { continue; }
+            }
+
+            Err(e) => {
+                println!("Error in streaming response: {e}");
+                break;
+            }
+        }
+    }
+    es.close();
+    Ok(())
+}
